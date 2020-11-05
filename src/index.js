d2b909fc000f0d8c564e4615310d469930c31932
Walison Teodoro <64755624+Wtheodoro@users.noreply.github.com>
2020-11-05 11:53:27 -0300

    up.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,42 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello World!'}) //retorn of json always in array or object
+app.get('/projects', (request, response) => {
+    //retorn of json always in array or object
+    return response.json([
+        'project ONE',
+        'project TWO',
+        'project THREE'
+    ]) 
 })
 
-app.listen(3333)
+// There is no problem to use the same path /projects but with an other method (get, post)
+app.post('/prohects', (request, response) =>{
+    return response.json([
+        'project ONE',
+        'project TWO',
+        'project THREE',
+        'project FOUR'
+    ])
+})
+
+app.put('/prohects/:id', (request, response) =>{
+    return response.json([
+        'project FIVE',
+        'project TWO',
+        'project THREE',
+        'project FOUR'
+    ])
+})
+
+app.delete('/prohects/:id', (request, response) =>{
+    return response.json([
+        'project TWO',
+        'project THREE',
+        'project FOUR'
+    ])
+})
+
+app.listen(3333, () => {
+    console.log('🚀️Back-end Started!')
+})
